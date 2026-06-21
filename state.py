da4407c00e0872b5058bc91db513a5af4145db7f
Harsh Kumar gaurav <93000616+HARSHKUMARGAURAV@users.noreply.github.com>
2026-06-21 17:52:24 +0530

    Add init_state function to initialize state

---
+++ state.py
@@ -0,0 +1,12 @@
+def init_state():
+    return {
+        "demographics": None,
+        "admission_date": None,
+        "discharge_date": None,
+        "diagnosis": [],
+        "med_admission": [],
+        "med_discharge": [],
+        "conflicts": [],
+        "missing": [],
+        "summary": None
+    }
